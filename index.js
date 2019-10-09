e759135fa2e82d1798b269d65a7ec9fcb30fae07
vivek bharda <vivekkbharda@gmail.com>
2019-10-09 11:23:47 +0530

    updated time zone and bug fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const http = require("http");
 const path = require("path");
 const https = require('https');
-
+const fs = require('fs');
 
 
 //import controllers
@@ -17,6 +17,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+//https server
+// const port = 4000;
+// var privateKey = fs.readFileSync('/var/www/html/attendence/ssl/privkey1.pem');
+// var certificate = fs.readFileSync('/var/www/html/attendence/ssl/fullchain1.pem');
+// var credentials = {key: privateKey, cert: certificate};
+// const secureServer = https.createServer(credentials,app);
+// secureServer.listen(port);
+// console.log("secure server started on 4000");
+
 mongoose.connect('mongodb://localhost:27017/muster_logs', {useNewUrlParser: true , useUnifiedTopology: true}  )
 .then(() => console.log("Congratulations you are connected to Database"))
 .catch(err => console.log(err));
@@ -27,3 +37,8 @@ app.use('/user' , userRoutes);
 
 
 app.listen(4000);
+
+
+
+
+
